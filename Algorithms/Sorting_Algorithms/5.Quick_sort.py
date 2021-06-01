bc91cf5ad6c02cd9e008a5862f783a22c7d27bfb
Dinakar Bijili <77189196+DinakarBijili@users.noreply.github.com>
2021-06-01 11:09:39 +0530

    Update 5.Quick_sort.py

--- Algorithms/Sorting_Algorithms/5.Quick_sort.py
+++ Algorithms/Sorting_Algorithms/5.Quick_sort.py
@@ -6,49 +6,27 @@
 # according to wheather their are less that or greaterthan the pivot. the sub-arrays are then sorted recursively. 
 
 #DIVIDE AND CONQUER METHOD
+def quick_sort(arr):
+   if len(arr) <= 1:
+      return arr
 
-#OTHER METHOD With Visuals
-# def quick_sort(arr):
-#    if len(arr) <= 1:
-#       return arr
-
-#    less_than_pivot = []
-#    greater_than_pivot = []
-#    pivot = arr[0] #first index
+   less_than_pivot = []
+   greater_than_pivot = []
+   pivot = arr[0] #first index
    
-#    for value in arr[1:]:
-#       if value <= pivot:
-#          less_than_pivot.append(value)
-#       else:
-#          greater_than_pivot.append(value)
+   for value in arr[1:]:
+      if value <= pivot:
+         less_than_pivot.append(value)
+      else:
+         greater_than_pivot.append(value)
 
-#    print("%15s %1s %-15s"% (less_than_pivot,pivot,greater_than_pivot))
-#    return quick_sort(less_than_pivot)+[pivot]+quick_sort(greater_than_pivot)
+   print("%15s %1s %-15s"% (less_than_pivot,pivot,greater_than_pivot))
+   return quick_sort(less_than_pivot)+[pivot]+quick_sort(greater_than_pivot)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
   
-#    arr  = [4,6,3,2,9,7,3,5]
-#    print(arr)
-#    result = quick_sort(arr)
-#    print(result)
+   arr  = [4,6,3,2,9,7,3,5]
+   print(arr)
+   result = quick_sort(arr)
+   print(result)
 
-def bubble_sort(arr):
-    n = len(arr)
-
-    # Traverse through all array elements
-    for i in range(n):
-
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
-
-            # Traverse the array from 0 to n-i-1
-            # If the element found is greater than the next element, swap
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-
-        print(arr)
-
-
-arr = [3, 8, 2, 5, 6, 1, 9, 4, 7]
-bubble_sort(arr)
-print("Sorted array is:", arr)
